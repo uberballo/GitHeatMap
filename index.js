4d294a1bcb5668ea07726e623ae2121091819785
Kasper Paavolainen <kasper.paavolainen@futurice.com>
2021-11-19 21:21:14 +0200

    Try to draw image

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const sdk = require('api')('@miro-ea/v1.11#1kqt1tkw4yylxx');
 
 try {
+
+  sdk.auth(`Bearer ${core.getInput('secret_key')}`);
+  sdk['rest-api-create-shape']({
+    data: {
+      content: 'Action shape',
+      shapeType: 'rectangle'
+    },
+    style: {
+      backgroundColor: '#fff9b1',
+      backgroundOpacity: '1.0',
+      fontFamily: 'arial',
+      fontSize: '14',
+      borderColor: '#1a1a1a',
+      borderWidth: '2.0',
+      borderOpacity: '1.0',
+      borderStyle: 'normal',
+      textAlign: 'center'
+    },
+    geometry: {
+      x: '0.0',
+      y: '0.0',
+      width: '200',
+      height: '200',
+      rotation: '0'
+    }
+  }, {board_id: `${core.getInput('board_id')}`})
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
